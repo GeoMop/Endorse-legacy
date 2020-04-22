bce3decfb9e594067b5d08a1a7f0c8adacd8a23f
Jan Brezina <jan.brezina@tul.cz>
2020-04-22 16:30:08 +0200

    Venv setup.

--- repository-model/setup_venv.py
+++ repository-model/setup_venv.py
@@ -1,19 +1,24 @@
+import pip
 import venv
 import os
 import pip
 import importlib
+import shutil
 
 env_dir='env'
-
 print("(SETUP ENV): Setup virtual environment for the project.")
 print("(SETUP ENV): Creating python environment 'env'.")
-venv.create(env_dir)
+builder = venv.EnvBuilder(system_site_packages=False, clear=False, symlinks=False, upgrade=False, with_pip=True)
+builder.create(env_dir)
+
 
 #source ./load_modules.sh
+# TODO: find a way how to do it directly from python without using shell
 
-activate_script = os.path.abspath(os.path.join(env_dir, "bin", "activate_this.py"))
-with open(activate_script) as fpy:
-    exec(f.read())
+env_python = os.path.join(env_dir, "bin", "python")
+#shutil.copyfile("activate_this.py", activate_script)
+#with open(activate_script) as fpy:
+    #exec(fpy.read())
 
 #print("(SETUP ENV): 'env' created python and pip in use:")
 #python --version
@@ -21,9 +26,9 @@ with open(activate_script) as fpy:
 #which pip
 
 print("(SETUP ENV): Installing necessary packages.")
+import sys, subprocess
 packages=['pyyaml', 'attrs', 'numpy', 'matplotlib', 'bgem']
-modules={}
-for p in packages:
-    pip.main(["install", "--prefix", env_dir, p])
-    modules[p] = importlib.import_module(p)
+subprocess.check_call([env_python, '-m', 'pip', 'install'] + packages)
+
+    
 
