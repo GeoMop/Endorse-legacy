46a9fdede7c637b9fd082e727d65933da17d7050
Paulie14 <pavelexner@gmail.com>
2022-11-21 16:05:58 +0100

    Fix setup for python3.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ tests/test_data/download.sh
 # Setup virtual environment for development
 
 
-python -m venv venv
+python3 -m venv venv
+# python3 -m venv --system-site-packages venv
 source venv/bin/activate
 
 python -m pip install -e ../bgem
