08f7992af00d12b2a2966a4b3de14fed8a478764
Paulie14 <pavelexner@gmail.com>
2022-11-22 15:25:07 +0100

    Fix setup venv for submodules.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # sudo apt install redis
 
 # download large data
-tests/test_data/download.sh
+# tests/test_data/download.sh
 
 # Setup virtual environment for development
 
@@ -13,7 +13,7 @@ python3 -m venv venv
 # python3 -m venv --system-site-packages venv
 source venv/bin/activate
 
-python -m pip install -e ../bgem
-python -m pip install -e ../bih
-python -m pip install -e ../redis-simple-cache-3k
+python -m pip install -e submodules/bgem
+python -m pip install -e submodules/bih
+python -m pip install -e submodules/redis-cache
 python -m pip install -r requirements.txt
