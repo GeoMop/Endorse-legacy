a2b52786a4a65e58e028438dc48c48dbe7b580d4
Paulie14 <pavelexner@gmail.com>
2022-11-29 15:23:43 +0100

    Compute porosity.

--- src/endorse/hm_simulation.py
+++ src/endorse/hm_simulation.py
@@ -8,13 +8,17 @@ import endorse.mesh_class
 from .common import File, sample_from_population, workdir, dotdict
 from .flow123d_simulation import endorse_2Dtest
 
+
 # 2D cross-section mesh is in xy plane with center in zero
 # target mesh cross-section is in yz plane
 class TunnelInterpolator:
-    def __init__(self, cfg_geom: dotdict, mesh: endorse.mesh_class.Mesh):
-        self._mesh = mesh
+    def __init__(self, cfg_geom: dotdict, flow_msh: endorse.mesh_class.Mesh, mech_msh: endorse.mesh_class.Mesh):
+        self._flow_msh = flow_msh
+        self._mech_msh = mech_msh
         self._cfg_geom = cfg_geom
-        bars = mesh.el_barycenters()
+
+        # precompute barycenter once
+        bars = flow_msh.el_barycenters()
         self._barycenters = (bars[0,:], bars[1,:])
 
     # Maps 'target_point' from the 3D tunnel to 'point' in 2D tunnel cross-section.
@@ -25,8 +29,61 @@ class TunnelInterpolator:
         points = (shifted_point[1,:], shifted_point[2:])
         return points
 
-    def test_interpolation(self):
-        field_values = self._mesh.get_p0_values("conductivity", time=365*24*3600)
+    def get_field_values(self, field_name, time):
+        try:    # flow fields
+            field_values = self._flow_msh.get_p0_values(field_name, time=time)
+            return field_values
+        except KeyError:
+            field_values = None
+
+        try:    # mech fields
+            field_values = self._mech_msh.get_p0_values(field_name, time=time)
+            return field_values
+        except KeyError:
+            field_values = None
+
+        raise KeyError("Unknown field name", field_name)
+
+    def interpolate_field(self, field_name, target_points, time):
+        field_values = self.get_field_values(field_name, time)
+        points = self.map_points(target_points)
+        # print(points)
+        vals = sp.interpolate.griddata(self._barycenters, field_values, points, method='linear')
+
+        return vals.squeeze()
+
+    def compute_porosity(self, hm_params, target_points, time, data=None):
+        # get parameters
+        biot = float(hm_params["biot_coefficient"])
+        init_pressure = float(hm_params["init_pressure"])
+        poisson_ratio = float(hm_params["poisson_ratio"])
+        young_modulus = float(hm_params["young_modulus"])
+
+        bulk_modulus = young_modulus/(3*(1-2*poisson_ratio))
+
+        # init_porosity = float(hm_params["init_porosity"])
+        # according to the considered relation for storativity
+        storativity = float(hm_params["storativity"])
+        w_compressibility = 4e-10  # water compressibility
+        delta_rho_g = 1 * 998 * 9.81
+        init_porosity = storativity / delta_rho_g + (biot*(biot-1)) / bulk_modulus
+        init_porosity = init_porosity / ((biot-1)/bulk_modulus + w_compressibility)
+        print("init_porosity", init_porosity)
+
+        if data is None:
+            interp_pressure = self.interpolate_field("pressure_p0", target_points, time)
+            interp_vol_strain = self.interpolate_field("displacement_divergence", target_points, time)
+        else:
+            interp_pressure, interp_vol_strain = data
+
+        pressure_diff = interp_pressure - init_pressure
+        strain_diff = interp_vol_strain  # Flow123d actually computes the difference to initial state
+        exponent = -(1-biot)/bulk_modulus * pressure_diff - strain_diff
+        porosity = biot + (init_porosity-biot) * np.exp(exponent)
+        return porosity
+
+    def test_conductivity(self):
+        field_values = self._flow_msh.get_p0_values("conductivity", time=365 * 24 * 3600)
         x = y = np.linspace(np.min(self._barycenters[0]), np.max(self._barycenters[1]), 500)
         X, Y = np.meshgrid(x, y)
         points = (X,Y)
@@ -54,15 +111,31 @@ class TunnelInterpolator:
         cb = fig.colorbar(s, ax=ax)
         plt.show()
 
-    def interpolate_field(self, field_name, target_points, time):
-        field_values = self._mesh.get_p0_values(field_name, time=time)
-        points = self.map_points(target_points)
-        # print(points)
-        vals = sp.interpolate.griddata(self._barycenters, field_values, points, method='linear')
+    def test_porosity(self, hm_params):
+        x = y = np.linspace(np.min(self._barycenters[0]), np.max(self._barycenters[1]), 500)
+        X, Y = np.meshgrid(x, y)
+        points = (X,Y)
 
-        return vals
+        time = 365 * 24 * 3600
+        field_pressure = self._flow_msh.get_p0_values("pressure_p0", time=time)
+        field_vol_strain = self._mech_msh.get_p0_values("displacement_divergence", time=time)
+        interp_pressure = sp.interpolate.griddata(self._barycenters, field_pressure, points, method='linear')
+        interp_vol_strain = sp.interpolate.griddata(self._barycenters, field_vol_strain, points, method='linear')
+        porosity = self.compute_porosity(hm_params, points, time, data=(interp_pressure, interp_vol_strain))
+        porosity = np.squeeze(porosity)
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(figsize=(8, 6))
 
+        ax.set_ylim(-50, 50)
+        ax.set_xlim(-50, 50)
+        ax.set_xlabel(r"$x$", fontsize=20)
+        ax.set_ylabel(r"$y$", fontsize=20)
 
+        # levels = np.array([])
+        c = ax.contourf(X, Y, porosity, cmap=plt.cm.viridis)
+        cb = fig.colorbar(c, ax=ax)
+
+        plt.show()
 
 def run_hm_simulation(config_dict: dotdict, i_sim: int, parameters: Dict[str,Union[int, float]]):
     # RUN THE MCMC SIMULATION
--- tests/test_hm_simulation.py
+++ tests/test_hm_simulation.py
@@ -25,20 +25,27 @@ def test_run_random_samples():
     hm_simulation.run_random_samples(cfg, 1)
 
 
+# @pytest.mark.skip
 def test_tunnel_interpolation():
     conf_file = os.path.join(script_dir, "test_data/config_homo_tsx.yaml")
     cfg = common.load_config(conf_file)
-    res_mesh = mesh_class.Mesh.load_mesh(File(os.path.join(script_dir, "test_data/flow_fields.msh")))
+    flow_msh = mesh_class.Mesh.load_mesh(File(os.path.join(script_dir, "test_data/flow_fields.msh")))
+    mech_msh = mesh_class.Mesh.load_mesh(File(os.path.join(script_dir, "test_data/mechanics_fields.msh")))
 
-    mesh_interp = hm_simulation.TunnelInterpolator(cfg.geometry, res_mesh)
+    mesh_interp = hm_simulation.TunnelInterpolator(cfg.geometry, flow_msh=flow_msh, mech_msh=mech_msh)
+    points = np.array([[10, 20], [4.375 / 2 + 0.01, 0], [1.1, 3.5 / 2 + 0.01 + 1.1]])
+    selected_time = 365 * 3600 * 24  # end_time of hm simulation
 
     field_name = "conductivity"
-    points = np.array([[10, 20], [4.375/2+0.01, 0], [1.1,  3.5/2+0.01+1.1]])
-    selected_time = 365 * 3600 * 24 # end_time of hm simulation
     print(field_name, points, selected_time)
     vals = mesh_interp.interpolate_field(field_name, points, time=selected_time)
     print(vals)
 
-    mesh_interp.test_interpolation()
+    mesh_interp.test_conductivity()
+
+    vals = mesh_interp.compute_porosity(cfg.tsx_hm_model.hm_params, points, time=selected_time)
+    print(vals)
+
+    mesh_interp.test_porosity(cfg.tsx_hm_model.hm_params)
 
     return
